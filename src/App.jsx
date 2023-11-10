20fd4a107e29470c660ef1665237209928da6eab
Abhishek Khatri <45210859+khatri7@users.noreply.github.com>
2023-11-09 19:37:05 -0500

    chore: added supabase js library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_PROJECT_URL,
+	import.meta.env.VITE_SUPABASE_PUBLIC_KEY
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [countries, setCountries] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		getCountries();
+	}, []);
+
+	async function getCountries() {
+		const { data } = await supabase.from("countries").select();
+		setCountries(data);
+	}
+
+	return (
+		<ul>
+			{countries.map((country) => (
+				<li key={country.name}>{country.name}</li>
+			))}
+		</ul>
+	);
 }
 
-export default App
+export default App;
